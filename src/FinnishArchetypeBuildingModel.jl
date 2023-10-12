35f54ee378531102ac77affb12c0f03fd678443b
Tasqu <topi.rasku@vtt.fi>
2023-10-12 11:40:23 +0300

    First working main program

--- src/FinnishArchetypeBuildingModel.jl
+++ src/FinnishArchetypeBuildingModel.jl
@@ -3,20 +3,23 @@ module FinnishArchetypeBuildingModel
 using FinnishBuildingStockData
 using ArchetypeBuildingModel
 
-# Exports for testscript
 export add_results!,
     archetype_building_processing,
+    BackboneInput,
     create_processed_statistics!,
     data_from_package,
     data_from_url,
     filter_module!,
+    GenericInput,
     import_data,
     initialize_result_classes!,
     run_input_data_tests,
     run_statistical_tests,
     run_structural_tests,
     solve_archetype_building_hvac_demand,
+    SpineOptInput,
     using_spinedb,
-    merge_data!
+    merge_data!,
+    write_to_url
 
 end # module FinnishArchetypeBuildingModel
